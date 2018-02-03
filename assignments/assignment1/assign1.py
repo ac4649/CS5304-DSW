76a28275e38262f91fcd81b2e0cf0f0ff51d042a
Adrien Cogny <ac2753@cornell.edu>
2018-02-03 12:19:14 -0500

    My own tester runs perfectly well. Also added a jupyter notebook.

--- assignments/assignment1/assign1.py
+++ assignments/assignment1/assign1.py
@@ -1,21 +1,66 @@
 # Adrien Cogny - Assignment 1 for Data Science in the Wild at Cornell Tech
+import pandas as pd
+import numpy as np
+
+from sklearn.datasets import fetch_rcv1
 from sklearn.neighbors import NearestNeighbors
+
+def load_labels(pathtoLabels):
+    DF = pd.read_csv(pathtoLabels,header=None);
+    return DF[0];
+
+def load_training_data():
+    dataSet = fetch_rcv1(subset='train')
+
+    return dataSet.data, dataSet.target, dataSet.sample_id
+
+def load_validation_data(validationData):
+
+    # This is taken from the homework FAQ and modified slightly.
+    test_data = fetch_rcv1(subset='test')
+    ids = pd.read_csv(validationData)
+    mask = np.isin(test_data.sample_id, ids)
+    validation_data = test_data.data[mask]
+    validation_target = test_data.target[mask]
+    return validation_data, validation_target, test_data
 
 class CS5304KNNClassifier():
 
     numNeighbors = 5;
 
     classifier = None;
-    def __init__(self):
-        # Initialization of the knn classifier should be done here
+    # def __init__(self,):
+    #     # Initialization of the knn classifier should be done here
+    #     self.classifier = NearestNeighbors(n_neighbors=self.numNeighbors);        
+    #     return
+    def __init__(self,newNumNeighbors = None):
+        if (type(newNumNeighbors) == int):
+            self.numNeighbors = newNumNeighbors;
         self.classifier = NearestNeighbors(n_neighbors=self.numNeighbors);        
-        return
 
-    def fit(fitX, fitY):
+    def train(fitX, fitY):
+
+        targetArray = dataSet['target'].toarray()
+        targetDF = pd.DataFrame(data=targetArray,index=dataSet.sample_id,columns=dataSet.target_names)
+        targetDF.head()
+
         self.classifier.fit(fitX,fitY);
         return
 
     def predict(predictX):
         return classifier.predict(predictX)
 
+    def score(data,labels):
+        return classifier.score(data,labels)
 
+
+
+class CS5304NBClassifier():
+
+    def __init__(self):
+        return
+
+class CS5304KMeansClassifier():
+
+    def __init__(self):
+        return
--- assignments/assignment1/assignment1Test.py
+++ assignments/assignment1/assignment1Test.py
@@ -4,29 +4,35 @@ print("Testing Assignment 1");
 print("Importing required libraries:");
 import numpy as np;
 import pandas as pd;
-
-print("Importing the dataset");
-from sklearn.datasets import fetch_rcv1
-dataSet = fetch_rcv1()
-
-print(dataSet.DESCR);
-print(dataSet.data.shape);
-print(dataSet.target.shape);
-print(dataSet.sample_id.shape);
-print(dataSet.target_names.shape);
+import argparse;
 
 print("Importing the classes");
-from assign1 import CS5304KNNClassifier;
+from assign1 import CS5304KNNClassifier
+# from assign1 import CS5304NBClassifier
+# from assign1 import CS5304KMeansClassifier
+from assign1 import load_labels, load_training_data, load_validation_data
+
+print("Argument Parsing");
+parser = argparse.ArgumentParser()
+parser.add_argument("--path_to_labels", default="labels.txt", type=str)
+parser.add_argument("--path_to_ids", default="validation.txt", type=str)
+parser.add_argument("--path_to_ks", default="ks.txt", type=str)
+parser.add_argument("--label", default=33, type=int)
+options = parser.parse_args()
+
+
+print("Importing dataSets");
+labels = load_labels(options.path_to_labels)
+train_data, train_target, _ = load_training_data()
+eval_data, eval_target, _ = load_validation_data(options.path_to_ids)
 
 print("Loading the Data into dataframe:");
-dataDF = pd.DataFrame(data=dataSet.data,index=dataSet.sample_id);
-dataSet.head();
-
 
 print("---------------------");
 print("Testing KNN Classification:");
 print("Initializing the classifier");
 classifier = CS5304KNNClassifier();
+
 
 # classifier.fit()
 

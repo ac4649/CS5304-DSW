79bfc9393023c5db99c7849db400ca3ac6df8271
Adrien Cogny <ac4649@nyu.edu>
2018-02-13 23:01:10 -0500

    Report up to one-hot encoding Fixed assign2 return for the generation of data still trying to do one-hot encoding with testingDAta.ipynb

--- assignments/assignment2/assign2.py
+++ assignments/assignment2/assign2.py
@@ -10,14 +10,14 @@ import itertools
 # Additional helper functions required for the code to run:
 # This function defines the column headers for the data frames
 def getColumnHeaders():
-    return pd.Series(data=['Index','label','integer_1','integer_2','integer_3',
+    return pd.Series(data=['label','integer_1','integer_2','integer_3',
                                  'integer_4','integer_5','integer_6','integer_7','integer_8','integer_9',
                                  'integer_10','integer_11','integer_12','integer_13','categorical_1',
                                  'categorical_2','categorical_3','categorical_4','categorical_5','categorical_6',
                                  'categorical_7','categorical_8','categorical_9','categorical_10','categorical_11',
                                  'categorical_12','categorical_13','categorical_14','categorical_15','categorical_16',
                                  'categorical_17','categorical_18','categorical_19','categorical_20','categorical_21',
-                                 'categorical_22','categorical_23','categorical_24','categorical_25','categorical_26'])
+                                 'categorical_22','categorical_23','categorical_24','categorical_25','categorical_26','Index'])
 
 # this function generateNIndecesFrom takes a range of Indeces and randomly takes n of them, returns a pandas Series object
 def generateNIndecesFrom(n, rangeOfIndeces):
@@ -43,7 +43,8 @@ def generateSubSet(file,dataFrame,indexValues,numRowsPerItteration,totalNumRows,
         curData = pd.read_table(file,skiprows = i * numRowsPerItteration, nrows = numRowsPerItteration,header=None)
         curData.index = [i for i in range(i*numRowsPerItteration,i*numRowsPerItteration + curData.shape[0])]
         totalNumRowsTraversed = totalNumRowsTraversed + curData.shape[0]
-
+        clear_output()
+#         print(curData.head())
 #         print(curData.shape)
 #         print(curData.index.shape)
 
@@ -51,13 +52,23 @@ def generateSubSet(file,dataFrame,indexValues,numRowsPerItteration,totalNumRows,
         curData.columns = column_headers
         
         curIndexRange = indexValues['Index'][(indexValues['Index'] < (i*numRowsPerItteration + numRowsPerItteration)) & (indexValues['Index'] > (i*numRowsPerItteration-1))]
-        curData = curData[curData['Index'].isin(curIndexRange)]
-
+#         print(curIndexRange)
+#         print(curData.head())
+#         print(curData['Index'])
+#         print(curData['Index'].isin(curIndexRange))
+        curData = curData[curData['Index'].isin(list(curIndexRange))] # this line isn't working for some reason
+#         print(curData.head())
+        
         dataFrame = pd.concat([dataFrame,curData])
         
-        # clear_output()
-        # print("Extraction Stats: " + str(dataFrame.shape[0]) + " percent: " + str(dataFrame.shape[0] / indexValues.shape[0] * 100) + "%")
-        # print("Document Stats: " + str(totalNumRowsTraversed) + " percent: " + str(totalNumRowsTraversed/totalNumRows*100) + "%")
+                
+        
+#         print(dataFrame.head())
+#         print(curData.head())
+        
+        
+        print("Extraction Stats: " + str(dataFrame.shape[0]) + " percent: " + str(dataFrame.shape[0] / indexValues.shape[0] * 100) + "%")
+        print("Document Stats: " + str(totalNumRowsTraversed) + " percent: " + str(totalNumRowsTraversed/totalNumRows*100) + "%")
         if (dataFrame.shape[0] - prevsize) > 500000:
             prevsize = dataFrame.shape[0]
 #             dataFrame.to_csv(frameSaveName)
@@ -76,70 +87,64 @@ def generateAndSaveSubset(file,dataFrame,indexValues,numRowsPerItteration,totalN
 
 def read_data(data_path, train_path, validation_path, test_path):
 
-    #train_path is the training ids
-    #validation_path is the validation ids
-    #test_path is the test ids
-
-
-    #Load or Generate the 2M indeces to use
+    print(data_path)
+    print(train_path)
+    print(validation_path)
+    print(test_path)
+    
+    #get the ids
     try:
-        twoMIndeces = pd.read_csv('2MIndeces.csv',squeeze = True)
-        
+        trainIndeces = pd.read_csv(train_path, header = None)
+        validationIndeces = pd.read_csv(validation_path, header = None)
+        testingIndeces = pd.read_csv(test_path, header = None)
     except:
-        # print("There were not 2000000 data points")
-        twoMIndeces = generateNIndecesFrom(2000000,range(0,45840617)) # this range is because there are this number of records in the training set.
-        twoMIndeces.to_csv('2MIndeces.csv',index=False,header=False)
-
-
-    #Load or Generate the 1M indeces for train, 250k validation and 750k test
-    try:
-        trainIndeces = pd.read_csv('train_ids.txt',squeeze = True)
-        validationIndeces = pd.read_csv('validation_ids.txt',squeeze = True)
-    except:
-        # print("There were not 1000000 data points")
+        print("There were not 1000000 data points")
         trainIndeces = generateNIndecesFrom(1000000,list(twoMIndeces['Index']))
         trainIndeces.to_csv('train_ids.txt',index=False,header=False)
 
         remainingIndeces = twoMIndeces['Index'][~twoMIndeces['Index'].isin(trainIndeces.values)]
         validationIndeces = generateNIndecesFrom(250000,list(remainingIndeces))
         validationIndeces.to_csv('validation_ids.txt',index=False,header=False)
-        
+
         testingIndeces = twoMIndeces['Index'][~(twoMIndeces['Index'].isin(trainIndeces.values) | twoMIndeces['Index'].isin(validationIndeces.values))]
         testingIndeces = generateNIndecesFrom(750000,list(testingIndeces))
-        testingIndeces.to_csv('testing_ids.txt',index=False,header=False)
-
-
+        testingIndeces.to_csv('test_ids.txt',index=False,header=False)
+    
+    trainIndeces.columns = ['Index']
+    validationIndeces.columns = ['Index']
+    testingIndeces.columns = ['Index']
+#     print(trainIndeces.head())
+#     print(validationIndeces.head())
+#     print(testingIndeces.head())
+    
     # Generate the actual data files
     column_headers = getColumnHeaders()
-    try:
-        train1M = pd.read_csv('train1M.csv',squeeze = True)
-    except:
-        # print("No 1M collection")
-        train1M = pd.DataFrame()
-        train1M = generateAndSaveSubset('dac/train.txt',train1M,trainIndeces,4000000,46000000,column_headers,'train1M.csv')
+    # print("No 1M collection")
+    train1M = pd.DataFrame()
+    train1M = generateSubSet(data_path,train1M,trainIndeces,4000000,46000000,column_headers)
+#     print(train1M.head())
+
+    # print("No 250k collection")
+    validation250k = pd.DataFrame()
+    validation250k = generateSubSet(data_path,validation250k,validationIndeces,4000000,46000000,column_headers)
 
 
-    try:
-        validation250k = pd.read_csv('validation250k.csv',squeeze = True)
-    except:
-        # print("No 250k collection")
-        validation250k = pd.DataFrame()
-        validation250k = generateAndSaveSubset('dac/train.txt',validation250k,validationIndeces,4000000,46000000,column_headers,'validation250k.csv')
-
-    try:
-        test750k = pd.read_csv('test750k.csv',squeeze = True)
-    except:
-        # print("No 750k collection")
-        test750k = pd.DataFrame()
-        test750k = generateAndSaveSubset('dac/train.txt',test750k,validationIndeces,4000000,46000000,column_headers,'test750k.csv')
+    # print("No 750k collection")
+    test750k = pd.DataFrame()
+    test750k = generateSubSet(data_path,test750k,validationIndeces,4000000,46000000,column_headers)
+    
+    
+    print(train1M.shape)
+    print(validation250k.shape)
+    print(test750k.shape)
 
     # train_data, train_target = np.zeros((1000000, 39)), np.zeros((1000000,))
     # validation_data, validation_target = np.zeros((250000, 39)), np.zeros((250000,))
     # test_data, test_target = np.zeros((750000, 39)), np.zeros((750000,))
 
 
-    # return train1M.to_array(), trainIndeces.to_array(), validation250k.to_array(), validation_target, test_data, test_target
-
+    return train1M[train1M.columns[1:40]], train1M['label'].values(), validation250k[validation250k.columns[1:40]], validation250k['label'].values(), test750k[test750k.columns[1:40]], test750k['label'].values()
+#     return 
 
 def preprocess_int_data(data, features):
     n = len([f for f in features if f < 13])

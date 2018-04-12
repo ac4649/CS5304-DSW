bf27e2b9f499b14976224c3a1b14c17342b6daad
Adrien Cogny <ac2753@cornell.edu>
2018-04-12 14:39:17 -0400

    working on tsne prediction

--- assignments/assignment6/assign6.py
+++ assignments/assignment6/assign6.py
@@ -11,8 +11,11 @@ import matplotlib.pyplot as plt
 
 from fashionmnist.model import FashionSimpleNet
 
+from fashionmnist.fashionmnist import FashionMNIST
+
 import torch
 from torch.utils.data import DataLoader
+import torchvision.transforms as transforms
 
 from tqdm import * # remove 
 
@@ -130,11 +133,15 @@ def applyISOMAP(XtrainDF, yTrainSeries, nFeatures):
 
 def applyTSNE(XtrainDF, yTrainSeries, nFeatures,imageName):
     XtrainDFStandard = StandardScaler().fit_transform(XtrainDF)
-    tsnemodel = TSNE(n_components=nFeatures)
+    tsnemodel = TSNE(n_components=nFeatures,verbose=True)
     XtrainPrincipalComponents = tsnemodel.fit_transform(XtrainDFStandard)
     XtrainPrincipalComponentsDF = pd.DataFrame(XtrainPrincipalComponents,columns=['pc'+str(i) for i in range(nFeatures)])
+    print(XtrainPrincipalComponentsDF.head())
+    print(yTrainSeries.head())
 
     resultsDF = pd.concat([XtrainPrincipalComponentsDF, yTrainSeries], axis = 1)
+
+    print(resultsDF.head())
 
     fig = plt.figure(figsize = (8,8))
     ax = fig.add_subplot(1,1,1) 
@@ -168,9 +175,13 @@ def applyTSNE(XtrainDF, yTrainSeries, nFeatures,imageName):
 
     return
 
+
+transform = transforms.Compose([transforms.ToTensor()])
+
+
 X_train, y_train = mnist_reader.load_mnist('data/fashion', kind='train')
 X_test, y_test = mnist_reader.load_mnist('data/fashion', kind='t10k')
-print(type(X_train))
+# print(X_train.shape)
 
 X_train = pd.DataFrame(X_train)
 y_train = pd.Series(y_train)
@@ -178,35 +189,76 @@ X_test = pd.DataFrame(X_test)
 y_test = pd.Series(y_test)
 
 
+
 # applyPCA(X_train,y_train,2)
 
 # #training isomap on 30% of the data
-subsetX_train = X_train.sample(frac=0.10)
+percentOfDataUsed = 0.1
+subsetX_train = X_train.sample(frac=percentOfDataUsed)
+# print(subsetX_train.shape)
+
 subsetY_train = y_train.iloc[subsetX_train.index.values]
 # print("Sample Size: " + str(subsetX_train.shape[0]))
 # applyISOMAP(subsetX_train,subsetY_train,2)
 
+applyTSNE(subsetX_train,subsetY_train,2,'tsne-raw.png')
 
-# applyTSNE(subsetX_train,subsetY_train,2,'tsne-raw.png')
+# applyTSNE(X_train,y_train,2,'tsne-raw.png')
 
 # using the trained model
 # load the model
+
+# reload the data and make it good for the classifier: (Taken from train.py given by TA and modified slightly)
+transform = transforms.Compose([ transforms.ToTensor()])
+kwargs = {}
+
+batch_size = 64
+num_workers = 4
+
+
+
+trainset = FashionMNIST('data', train=True, download=True, transform=transform)
+train_loader = DataLoader(trainset, batch_size=batch_size,
+                        shuffle=True, num_workers=num_workers, **kwargs)
+                        
+valset = FashionMNIST('data', train=False, transform=transform)
+val_loader = DataLoader(valset, batch_size=batch_size,
+                        shuffle=False, num_workers=num_workers, **kwargs)
+
+# end of TA code
+
 model = FashionSimpleNet()
 state_dict = torch.load('fashionmnist/saved-models/FashionSimpleNet-run-1.pth.tar')['state_dict']
 model.load_state_dict(state_dict)
 
 model.classifier = torch.nn.Sequential(*list(model.classifier.children())[:-1]) #remove the last layer
 model.eval()
-print(model)
-# print(subsetX_train[:0])
-dataLoader = DataLoader(subsetX_train.values, batch_size=64, shuffle=True, num_workers=1)
-
-for i, X in tqdm(enumerate(dataLoader), desc='computing', total=subsetX_train.shape[0]/64):
-    subsetX_modeled = model(torch.autograd.Variable(X, volatile=True))
-# print(subsetX_modeled.shape)
-# applyTSNE(subsetX_train,subsetY_train,2,'tsne-resnet.png')
 
 
+size = train_loader.dataset.train_data.shape[0]
+batch_size = train_loader.batch_size
+num_batches = size // batch_size
+num_batches = num_batches if train_loader.drop_last else num_batches + 1
 
 
+outputXs = pd.DataFrame()
+outputYs = pd.Series()
 
+for i , (X,y) in tqdm(enumerate(train_loader), desc='Predicting', total=num_batches*percentOfDataUsed):
+    X = torch.autograd.Variable(X, volatile=True)
+    netX = model(X)
+    # print(netX.data.cpu().numpy())
+    curXs = pd.DataFrame(netX.data.cpu().numpy())
+    outputXs = outputXs.append(curXs,ignore_index=True)
+    outputYs = outputYs.append(pd.Series(y.cpu().numpy()))
+    
+    if i > percentOfDataUsed*num_batches:
+
+        break
+print(outputXs.shape)
+print(outputYs.shape)
+
+outputXs.to_csv('tsnetPredictedXs.csv')
+outputYs.to_csv('tsnetPredictedYs.csv')
+
+applyTSNE(outputXs,outputYs,2,'tsne-predicted.png')
